be579490f614897574d19712f5441dcee0d183b0
citrino88 <almeida.citrino@gmail.com>
2024-05-29 15:56:10 -0300

    Update conecta.php

--- inc/conecta.php
+++ inc/conecta.php
@@ -8,19 +8,21 @@ $usuario = "root";
 $senha = "";
 $banco = "microblog_dayane";
 
-// Função para conexão com o servidor de banco de dados, colocamos dentro de uma variável abaixo
+// Função para conexão com o servidor de banco de dados, colocamos dentro de uma variável abaixo conexao
 $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
 
 //
 mysqli_set_charset($conexao, "utf8");
 
 /* Fazendo um teste de conexão */
-if ( !$conexao ) {
+/* if ( !$conexao ) {
     // Deu problema? "Mate/Pare" a aplicação!
     die("Deu ruim: " .mysqli_connect_error());
 } else {
     echo "Beleza, conectado...";
-}
+} 
+
+comentamos esse if pois serviu apenas para testar se deu certo*/
 
 
 ?>
