5339eaeea21d5fb4f8335c387c63e1ddb564f12a
citrino88 <almeida.citrino@gmail.com>
2024-05-29 15:30:47 -0300

    Importante!! Conexão php com o BD

---
+++ inc/conecta.php
@@ -0,0 +1,26 @@
+<?php 
+/* Script de conexão ao servidor de banco de dados
+Necessários para que o Microblog possa usar o MySQL */
+
+// Parâmeros para conexão
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "microblog_dayane";
+
+// Função para conexão com o servidor de banco de dados, colocamos dentro de uma variável abaixo
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+//
+mysqli_set_charset($conexao, "utf8");
+
+/* Fazendo um teste de conexão */
+if ( !$conexao ) {
+    // Deu problema? "Mate/Pare" a aplicação!
+    die("Deu ruim: " .mysqli_connect_error());
+} else {
+    echo "Beleza, conectado...";
+}
+
+
+?>
