ba35afb07b38753371ea9959bc50de28e9445a00
citrino88 <almeida.citrino@gmail.com>
2024-06-04 16:44:44 -0300

    excluir um usuario

---
+++ admin/usuario-exclui.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../inc/funcoes-usuarios.php";
+// OBTER O ID DO USUÁRIO QUE SERÁ EXCLUIDO
+
+$id = $_GET['id'];
+
+// CHAMAR/ EXECUTOR A FUNÇÃO QUE IRÁ FAZER O DELETE
+
+excluirUsuario($conexao, $id);
+
+// REDIRECIONAR PARA A PÁGINA DE USUÁRIOS
+header("location:usuarios.php");
+?>
+
