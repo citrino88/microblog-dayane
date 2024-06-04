8b2a20508cc54210a46f5555f32a0173cdf68153
citrino88 <almeida.citrino@gmail.com>
2024-06-04 17:29:01 -0300

    colocando somente para acesso a usuário credenciado

---
+++ inc/funcoes-sessao.php
@@ -0,0 +1,31 @@
+<?php
+
+/* Sessões no PHP
+Funcionalidade usada para o controle de acesso à determinadas páginas e/ou recursos do site.
+
+Exemplos: área administrativa, painel de controle, área de cliente/aluno etc.
+
+Nestas áreas o acesso só é possível mediante alguma forma de autenticação. Exemplos: login/senha, digital, facial etc. */
+
+// Verificando se NÃO há/existe uma sessão em funcionamento
+
+if( !isset($_SESSION) ){
+    // Se não tem, então inicie uma sessão
+    session_start();
+}
+
+function verificaAcesso(){
+    /* Se NÃO EXISTIR uma variável de sessão chamada "id" (baseada nos ids usuários do banco de dados), então significa que o usuário NÃO ESTÁ LOGADO. */
+    if(!isset($_SESSION['id'])){
+        // Portanto, destruímos a sessão
+        session_destroy();
+
+        // Fazemos o usuário voltar para a página login
+        header("location:../login.php");
+
+        // Paramos qualquer outra execução/processamento
+        exit; // ou die()
+    }
+}
+
+?>
