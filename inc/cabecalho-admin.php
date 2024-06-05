220892558a41d52f577923bdd0676fb391f49d8d
citrino88 <almeida.citrino@gmail.com>
2024-06-05 17:18:19 -0300

    usuarios só aparece para Admin

--- inc/cabecalho-admin.php
+++ inc/cabecalho-admin.php
@@ -42,10 +42,13 @@ $pagina = basename($_SERVER['PHP_SELF']);
             <li class="nav-item">
                 <a class="nav-link" href="meu-perfil.php">Meu perfil</a>
             </li>
-                       
+            
+            <!-- Se for usuário do tipo admin, então mostre o link abaixo -->
+            <?php if($_SESSION['tipo'] == 'admin'){?>
             <li class="nav-item">
                 <a class="nav-link" href="usuarios.php">Usuários</a>
             </li>
+            <?php }?>
             
             <li class="nav-item">
                 <a class="nav-link" href="noticias.php">Notícias</a>
